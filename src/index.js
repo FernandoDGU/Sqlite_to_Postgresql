b35202e8406d0b9f1c4d85aff93932609faabb16
FernandoDGU <66134044+FernandoDGU@users.noreply.github.com>
2023-06-21 15:05:17 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,7 @@ require('dotenv').config();
 
 
 const sqliteConn = new sqlite3.Database('src/BusAccesosDB.db')
-// dev
-// const pgConn = new Client({
-//   user: 'ixoqltkgxmsohk',
-//   host: 'ec2-3-230-122-20.compute-1.amazonaws.com',
-//   database: 'd99cim5pbrplh8',
-//   password: '76e6024732f283c7e13390b5ffcf4e5b297910b14256d974fc6d7975be8ece89',
-//   port: 5432,
-//   ssl: { rejectUnauthorized: false }
-// });
+
 
 // prod
 const pgConn = new Client({
@@ -28,11 +20,6 @@ pgConn.connect();
 
 let values = [];
 
-// sqliteConn.serialize(function () {
-//   sqliteConn.all("select name from sqlite_master where type='table'", function (err, tables) {
-//       console.log(tables);
-//   });
-// });
 sqliteConn.all(`SELECT access_date,balance_new, balance_old, qr_creation_date as creation_date, device_id, discount_amount, discount_concept, latitude, longitude, passengers
 ,payment_company, payment_company_id, payment_method, payment_method_id, qr_version, subtotal, ticket_id, user_category, total, user_id, user_profile, user_title, no_bank, bank, no_bank_flag
 FROM BusAccessTable`, (err, rows) => {
@@ -43,8 +30,8 @@ FROM BusAccessTable`, (err, rows) => {
 
   // Utiliza una consulta INSERT en la base de datos PostgreSQL para insertar los datos
   rows.forEach((row) => {
-    if(row.device_id == 'T2fTLkXRhmUWvuNx3S') {
-        row.device_id = 67
+    if(row.device_id == 'DDnrdK4iyl4D4fPfsl') {
+        row.device_id = 241
     }
 
     if(row.payment_company == 'MIA') {
@@ -69,14 +56,10 @@ FROM BusAccessTable`, (err, rows) => {
       row.payment_method = 2
     }
 
-    if(row.device_id == 'T2fTLkXRhmUWvuNx3S') {
-      row.device_id = 67
+    if(row.device_id == 'DDnrdK4iyl4D4fPfsl') {
+      row.device_id = 241
     }
 
-    // values = [
-    //   row.access_date, row.balance_new, row.balance_old, row.creation_date, row.device_id, row.discount_amount, row.discount_concept, row.latitude, row.longitude, row.passengers,
-    //   row.payment_company, row.payment_company_id, row.payment_method, row.payment_method_id, row.qr_version, row.subtotal, row.ticket_id, row.user_category, row.total, row.user_id, row.user_profile, row.user_title,
-    // ];
 
     values.push(({
       access_date: row.access_date, 
@@ -106,18 +89,6 @@ FROM BusAccessTable`, (err, rows) => {
       no_bank_flag: row.no_bank_flag
     }))
 
-    // console.log(values);
-    // const query = `INSERT INTO tra_bus_access (access_date, balance_new, balance_old, creation_date, device_id, discount_amount, discount_concept,
-    //     latitude, longitude, passengers, payment_company, payment_company_id, payment_method, 
-    //     payment_method_id, qr_version, subtotal, ticket_id, user_category, total, user_id, user_profile, user_title) 
-    //     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22)`;
-    // pgConn.query(query, values, (err) => {
-    //   if (err) {
-    //     console.error(err);
-    //     return;
-    //   }
-    //   console.log(`Inserted row with ID ${row.id}`);
-    // });
   });
 
   console.log(values);
@@ -157,14 +128,5 @@ FROM BusAccessTable`, (err, rows) => {
   // Cierra las conexiones
          
 });
-
-// sqliteConn.close();
-// pgConn.end();
-
-
-
-
-
-
 
 // node src/index.js
